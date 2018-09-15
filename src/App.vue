62aa17a8ead6d20b37376a52b99aea53143750c3
Xiang9527 <v718030@gmail.com>
2018-09-15 15:31:53 +0800

    vuex-counter

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,44 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Routing</h1>
+                <h1>Vuex</h1>
+                <app-result></app-result>
+                <app-another-result></app-another-result>
                 <hr>
-                <router-view name="header-top"></router-view>
-                <router-view></router-view>
-                <router-view name="header-bottom"></router-view>
+                <app-counter></app-counter>
+                <hr>
+                <app-another-counter></app-another-counter>
+                <hr>
+                <input type="text" v-model="value">
+                <p>{{value}}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Header from './components/Header.vue';
+    import Counter from './components/Counter.vue';
+    import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
+    import AnotherCounter from './components/AnotherCounter.vue';
+
     export default {
-        components:{
-            appHeader:Header
+        computed:{
+            value: {
+                get(){
+                    return this.$store.getters.value;
+                },
+                set(value){
+                    this.$store.dispatch('updateValue',value);
+                }
+            }
+        },
+        components: {
+            appCounter: Counter,
+            appResult: Result,
+            appAnotherResult: AnotherResult,
+            appAnotherCounter: AnotherCounter,
         }
     }
 </script>
 
-<style>
-</style>
--- src/main.js
+++ src/main.js
@@ -1,28 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import {routes} from './routes'
 
-Vue.use(VueRouter)
-const router = new VueRouter({
-  routes,
-  mode: 'history',
-  scrollBehavior(to,from,savedPosition){
-    if(savedPosition){
-      return savedPosition;
-    }
-    if(to.hash){
-      return {selector:to.hash};
-    }
-  }
-});
+import {store} from './store/store';
 
-router.beforeEach((to,from,next) => {
-  next();
-});
- 
 new Vue({
   el: '#app',
-  router,
+  store:store,
   render: h => h(App)
 })
